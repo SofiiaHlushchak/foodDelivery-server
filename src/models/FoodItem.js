f67042f0d08a1f8c6aa6ab5e945d8529d5ffeb40
SofiiaHlushchak <glushchak02@gmail.com>
2025-01-28 20:10:04 +0100

    feat: Set up database connection and add models for Category, FoodItem, and Restaurant

---
+++ src/models/FoodItem.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const foodItemSchema = new mongoose.Schema({
+    id: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    rating: { type: Number, required: true },
+    feedbacks: { type: Number, required: true },
+    ingredients: [String],
+    addons: [
+        {
+            name: { type: String, required: true },
+            price: { type: Number, required: true },
+            countable: { type: Boolean, required: true },
+        },
+    ],
+    imgUrl: { type: String, required: true },
+    restaurantId: { type: String, required: true },
+});
+
+const FoodItem = mongoose.model("FoodItem", foodItemSchema);
+
+module.exports = FoodItem;
--- src/server.js
+++ src/server.js
@@ -1,16 +1,12 @@
 const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const connectDB = require("./db");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.log("Error connecting to MongoDB:", err));
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("Hello, world!");
