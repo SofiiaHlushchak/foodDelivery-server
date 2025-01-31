6bc4814985466aea180eafcb7598667cd6035254
SofiiaHlushchak <glushchak02@gmail.com>
2025-01-31 07:57:22 +0100

    feat: add API endpoints to fetch restaurants and dishes by ID

--- src/controllers/restaurantController.js
+++ src/controllers/restaurantController.js
@@ -8,3 +8,18 @@ export const getRestaurants = async (req, res) => {
         res.status(500).json({ message: "Error fetching restaurants", error });
     }
 };
+
+export const getRestaurantById = async (req, res) => {
+    const { restaurantId } = req.params;
+    try {
+        const restaurant = await Restaurant.findById(restaurantId);
+
+        if (!restaurant) {
+            return res.status(404).json({ message: "Restaurant not found." });
+        }
+
+        res.status(200).json(restaurant);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching restaurant", error });
+    }
+};
