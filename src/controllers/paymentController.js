6dd76ee491a3b9d12139784a694b7c166d2ee513
SofiiaHlushchak <glushchak02@gmail.com>
2025-02-26 16:38:57 +0100

    feat: get cards

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -31,3 +31,31 @@ export const savePaymentCard = async (req, res) => {
         res.status(500).json({ message: "Error saving payment card", error });
     }
 };
+
+export const getUserPaymentCards = async (req, res) => {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({ message: "Token not provided" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decoded.userId;
+
+        const paymentCards = await PaymentCard.find({ userId });
+
+        if (paymentCards.length === 0) {
+            return res
+                .status(404)
+                .json({ message: "No payment cards found for this user" });
+        }
+
+        res.status(200).json(paymentCards);
+    } catch (error) {
+        res.status(500).json({
+            message: "Error retrieving payment cards",
+            error,
+        });
+    }
+};
