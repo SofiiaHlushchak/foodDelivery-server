dc9c32c4765d582285c6e11a9cc43bc8c99d10dc
Hlushchak Sofiia <52669896+SofiiaHlushchak@users.noreply.github.com>
2025-03-06 08:01:30 +0100

    feat: create payment link (#13)

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -1,5 +1,8 @@
 import PaymentCard from "../models/PaymentCard.js";
 import jwt from "jsonwebtoken";
+import stripeLib from "stripe";
+
+const stripe = stripeLib(process.env.STRIPE_SECRET_KEY);
 
 export const savePaymentCard = async (req, res) => {
     const { cardNumber, expirationDate } = req.body;
@@ -57,5 +60,33 @@ export const getUserPaymentCards = async (req, res) => {
             message: "Error retrieving payment cards",
             error,
         });
+    }
+};
+
+export const createPaymentLink = async (req, res) => {
+    try {
+        const { totalPrice } = req.body;
+
+        const price = await stripe.prices.create({
+            unit_amount: Math.round(totalPrice * 100),
+            currency: "usd",
+            product_data: {
+                name: "Food Delivery",
+            },
+        });
+
+        const paymentLink = await stripe.paymentLinks.create({
+            line_items: [
+                {
+                    price: price.id,
+                    quantity: 1,
+                },
+            ],
+        });
+
+        res.json({ url: paymentLink.url });
+    } catch (error) {
+        console.error("Error creating payment link:", error);
+        res.status(500).json({ error: "Error creating payment link" });
     }
 };
--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -2,11 +2,12 @@ import express from "express";
 import {
     getUserPaymentCards,
     savePaymentCard,
+    createPaymentLink,
 } from "../controllers/paymentController.js";
 
 const router = express.Router();
 
-router.post("/save", savePaymentCard);
 router.get("/", getUserPaymentCards);
-
+router.post("/save", savePaymentCard);
+router.post("/create-payment-link", createPaymentLink);
 export default router;
